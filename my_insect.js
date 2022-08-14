96fe97e21fc12c2fdf3799f57be12923e2eb33c4
Anand Balaji <76686299+anand-2@users.noreply.github.com>
2022-08-14 14:18:30 +0530

    task 1

---
+++ my_insect.js
@@ -0,0 +1,74 @@
+function my_insect()
+{
+    var insectPos = [1,2];
+    var insectDirection = "N";
+    var Commands = "LFLFLFLFF"
+
+    for(let i=0;i<Commands.length;i++)
+    {
+        if(insectDirection === "N" && Commands.charAt(i) === "L" )
+        {
+            insectDirection = "W",
+            insectPos[0]--
+        }
+        else if(insectDirection === "N" && Commands.charAt(i) === "F" )
+        {
+            insectPos[1]++
+        }
+        else if(insectDirection === "N" && Commands.charAt(i) === "R" )
+        {
+            insectDirection = "E",
+            insectPos[0]++
+        }
+
+        else if(insectDirection === "S" && Commands.charAt(i) === "L" )
+        {
+            insectDirection = "E",
+            insectPos[0]++
+        }
+        else if(insectDirection === "S" && Commands.charAt(i) === "F" )
+        {
+            insectPos[1]--
+        }
+        else if(insectDirection === "S" && Commands.charAt(i) === "R" )
+        {
+            insectDirection = "W",
+            insectPos[0]--
+        }
+
+        else if(insectDirection === "E" && Commands.charAt(i) === "L" )
+        {
+            insectDirection = "N",
+            insectPos[1]++
+        }
+        else if(insectDirection === "E" && Commands.charAt(i) === "F" )
+        {
+            insectPos[0]++
+        }
+        else if(insectDirection === "E" && Commands.charAt(i) === "R" )
+        {
+            insectDirection = "S",
+            insectPos[1]--
+        }
+
+        else if(insectDirection === "W" && Commands.charAt(i) === "L" )
+        {
+            insectDirection = "S",
+            insectPos[1]--
+        }
+        else if(insectDirection === "W" && Commands.charAt(i) === "F" )
+        {
+            insectPos[0]--
+        }
+        else if(insectDirection === "W" && Commands.charAt(i) === "R" )
+        {
+            insectDirection = "N",
+            insectPos[1]++
+        }
+    }
+
+    return "" + insectDirection +" " +  insectPos[0] + " " +  insectPos[1];
+
+}
+
+console.log(my_insect())
