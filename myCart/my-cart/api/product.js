77e75232130911d495d10098c9ab556d919b4902
Anand Balaji <76686299+anand-2@users.noreply.github.com>
2022-08-31 00:42:06 +0530

    added login

--- myCart/my-cart/api/product.js
+++ myCart/my-cart/api/product.js
@@ -85,7 +85,6 @@ var getCart = async function(connPool){
     });
 }
 
-
 var insertIntoCart = async function (connPool, product_details) {
 
     return new Promise((resolve, reject) => {
@@ -202,4 +201,108 @@ var DeleteCart = async function (connPool, product_details) {
     });
 }
 
-module.exports = [getAllProduct,getCart,insertIntoCart, updateCart,DeleteCart];
+var checkUserExist = async function(connPool , User){
+    return new Promise((resolve,reject) =>{
+        let resp = {
+            data: undefined,
+            error: true,
+            message: "Username or email already exists"
+        }
+        connPool.query('Select * from "user" where username = $1 or email = $2 ',[User.username , User.email] ,(error,results) =>{
+            if(error)
+            {
+                resp.error = true;
+                resp.message = error.message;
+                reject(resp)
+            }
+            else{
+                if(results.rows.length==0)
+                {
+                    resp.error = false;
+                    resp.message = "Username and email are available";
+                    resolve(resp);
+                }
+                else
+                {
+                    reject(resp)
+                }
+            }
+        })
+    });
+}
+
+var insertUser = async function(connPool , User){
+    return new Promise((resolve,reject)=>{
+        let resp = {
+            data: undefined,
+            error: true,
+            message: "Something went wrong"
+        }
+        connPool.query('Insert into "user"(username,email,firstname,lastname,pass,address) values($1,$2,$3,$4,$5,$6) RETURNING user_id',[User.username,User.email,User.firstname,User.lastname,User.pass,User.address] , (error,results)=>{
+            if(error)
+            {
+                resp.error = true;
+                resp.message = error.message
+                reject(resp)
+            }
+            else
+            {
+              
+                resp.data = results.rows[0].user_id;
+                resp.error = false;
+                resp.message = "User added succesfully"
+                resolve(resp)
+
+            }
+
+        })
+
+    })
+
+}
+
+
+var getCredentials = async function(connPool , email_address){
+    return new Promise((resolve, reject) => {
+        let resp = {
+            data: undefined,
+            error: true,
+            message: "Something went wrong"
+        }
+
+        connPool.query('SELECT email,pass FROM "user" WHERE email = $1',[email_address] , (error, results) => {
+           
+            if(!error && !results.rows[0])
+            {
+                resp = {
+                    error: true,
+                    message: "Email not found"
+                }
+                reject(resp)
+                return;
+            }
+            if (error) {
+                resp = {
+                    error: true,
+                    message: error.message
+                }
+                reject(resp)
+            } else {
+                resp = {
+                    data: {
+                        email :  results.rows[0].email,
+                        pass :  results.rows[0].pass
+                    },
+                    error: false,
+                    message: "User detailed fetched"
+                }
+            }
+
+            resolve(resp)
+        })
+    });
+}
+
+
+
+module.exports = [getAllProduct,getCart,insertIntoCart, updateCart,DeleteCart,checkUserExist,insertUser,getCredentials];
--- myCart/my-cart/api/routes.js
+++ myCart/my-cart/api/routes.js
@@ -4,8 +4,10 @@ const {
 } = require("pg")
 const client = new Client();
 var express = require("express");
+var bcrypt = require("bcrypt")
 
-const [getAllProduct,getCart,insertIntoCart,updateCart,DeleteCart] = require("./product");
+const [getAllProduct,getCart,insertIntoCart,updateCart,DeleteCart,checkUserExist,insertUser,getCredentials] = require("./product");
+const jwt = require("jsonwebtoken");
 var app = express.Router();
 
 const pool = new Pool({
@@ -16,20 +18,52 @@ const pool = new Pool({
     port: process.env.DB_PORT,
 })
 
-app.get("/product" ,async(req,res)=>{
-
-    await getAllProduct(pool)
+app.get("/product" ,verifyToken,async(req,res)=>{
+    jwt.verify(req.token , process.env.SECRET,async(err,authData)=>{
+        if(err)
+        {
+            res.send(err)
+        }
+        else{
+            await getAllProduct(pool)
             .then(resp => {
                 res.send(resp)
             })
             .catch(err => {
                 res.send(err)
             })
+        }
+    })   
 }) 
 
-app.get("/cart" ,async(req,res)=>{
+app.get("/cart" ,verifyToken,async(req,res)=>{
+ jwt.verify(req.token , process.env.SECRET , async(err,authData)=>{
+    if(err){
+        res.send(err)
+    }
+    else
+    {
+        await getCart(pool)
+        .then(resp => {
+            res.send(resp)
+        })
+        .catch(err => {
+            //
+            res.send(err)
+        })
+    }
+ })   
+}) 
 
-    await getCart(pool)
+app.post("/cart" ,verifyToken,async(req,res)=>{
+
+    jwt.verify(req.token , process.env.SECRET , async (err,authData)=> {
+        if(err)
+        {
+            res.send(err)
+        }
+        else {
+            await insertIntoCart(pool,req.body)
             .then(resp => {
                 res.send(resp)
             })
@@ -37,24 +71,19 @@ app.get("/cart" ,async(req,res)=>{
                 //
                 res.send(err)
             })
-}) 
-
-app.post("/cart" ,async(req,res)=>{
-
-    await insertIntoCart(pool,req.body)
-            .then(resp => {
-                res.send(resp)
-            })
-            .catch(err => {
-                //
-                res.send(err)
-            })
+        }
+    })
+    
 }) 
   
 
-app.put("/cart" ,async(req,res)=>{
-
-    await updateCart(pool,req.body)
+app.put("/cart" ,verifyToken,async(req,res)=>{
+    jwt.verify(req.token,process.env.SECRET, async(err,authData)=>{
+        if(err){
+            res.send(err)
+        }
+        else{
+            await updateCart(pool,req.body)
             .then(resp => {
                 res.send(resp)
             })
@@ -62,11 +91,18 @@ app.put("/cart" ,async(req,res)=>{
                 //
                 res.send(err)
             })
+        }
+    })
+    
 }) 
   
-app.delete("/cart" ,async(req,res)=>{
-
-    await DeleteCart(pool,req.query)
+app.delete("/cart" ,verifyToken,async(req,res)=>{
+    jwt.verify(req.token, process.env.SECRET, async(err,authData)=>{
+        if(err){
+            res.send(err)
+        }
+        else{
+            await DeleteCart(pool,req.query)
             .then(resp => {
                 res.send(resp)
             })
@@ -74,7 +110,80 @@ app.delete("/cart" ,async(req,res)=>{
                 //
                 res.send(err)
             })
+        }
+    })
 }) 
+
+app.post("/register",async(req,res)=>{
+    let User = req.body;
+    const hash = bcrypt.hashSync(User.pass, 10);
+    User.pass = hash
+    await checkUserExist(pool,User)
+        .then(async resp=>{
+            await insertUser(pool,User)
+                .then(resp=>{
+                    res.send(resp)
+                })
+                .catch(resp=>{
+                    res.send(err)
+                })
+            
+        })
+        .catch(err=>{
+            res.send(err)
+        })
+})
+
+app.post("/login",async(req,res) =>{
+    const enteredEmail = req.body.email;
+    const enteredpass = req.body.pass;
+    let result = {
+        error: true,
+        message: "Login failed. Please check username or password",
+        token : undefined
+    }
+    await getCredentials(pool,enteredEmail)
+        .then(resp=> {
+           
+            bcrypt.compare(enteredpass, resp.data.pass).then((success) => {
+                console.log(enteredpass)
+             if(success)
+             {                
+                jwt.sign({resp}, process.env.SECRET, (error,token) =>{
+                    result.error = false;
+                    result.message = "Login successful";
+                    result.token = token
+                    res.send(result)
+                    return
+                })
+             }
+             else{
+                res.send(result)
+                return
+             }
+           
+            })
+            .catch(err =>{
+                res.send(err)
+            })
+        })   
+})
+
+function verifyToken(req,res,next){
+    const bearerHeader = req.headers['authorization'];
+    if(typeof bearerHeader!== "undefined")
+    {
+        const bearer = bearerHeader.split(" ")
+        const bearerToken = bearer[1];
+        req.token = bearerToken ;
+        next();
+    }
+    else{
+        res.json("Forbidden Access")
+    }
+}
+
+
   
 
 module.exports = app;
